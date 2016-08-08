283b22e6294432dfb10bf8902f4940a18358a0fe
Tapani Otala <github@tjotala.com>
2016-08-08 14:49:58 -0700

    Substantial re-factoring to use UI AngularJS for templatized modal dialogs. Also adds font size settings as the first of possibly many settings.

--- web_ui/scripts/solo.js
+++ web_ui/scripts/solo.js
@@ -1,18 +1,35 @@
-var app = angular.module('App', []);
+var app = angular.module('App', [ 'ui.bootstrap' ]);
 
-app.controller('DocumentStore', function DocumentStore($scope, $http) {
-	$scope.list = function() {
+app.controller('DocumentStore', function DocumentStore($scope, $http, $uibModal) {
+	$scope.getById = function(id) {
+		return document.getElementById(id);
+	}
+
+	var contentStyle = window.getComputedStyle($scope.getById("content"));
+	$scope.defaults = {
+		text: {
+			font: contentStyle.getPropertyValue('font-family'),
+			size: parseFloat(contentStyle.getPropertyValue('font-size')),
+			minSize: 10, // px
+			maxSize: 50  // px
+		}
+	};
+	$scope.text = JSON.parse(JSON.stringify($scope.defaults.text)); // hokey JS way to dupe an object
+	$scope.development = true;
+	$scope.settings = false;
+
+	$scope.list = function(callback) {
 		$http.get("/api/files/").then(
 			function success(response) {
-				$scope.docs = response.data;
+				callback(response.data);
 			},
 			function failure(response) {
-				$scope.docs = [ ];
+				callback([ ]);
 			}
 		);
 	};
 
-	$scope.get = function(name) {
+	$scope.load = function(name) {
 		$http.get("/api/files/" + encodeURIComponent(name)).then(
 			function success(response) {
 				$scope.name = name;
@@ -20,7 +37,6 @@ app.controller('DocumentStore', function DocumentStore($scope, $http) {
 				$scope.size = response.headers("Content-Length");
 				$scope.modified = new Date(Date.parse(response.headers("Last-Modified")));
 				$scope.dirty = false;
-				$scope.close();
 				$scope.setFocus("content");
 			},
 			function failure(response) {
@@ -29,12 +45,14 @@ app.controller('DocumentStore', function DocumentStore($scope, $http) {
 		);
 	};
 
-	$scope.set = function(name, content) {
-		$http.put("/api/files/" + encodeURIComponent($scope.doc.name), $scope.doc.content).then(
+	$scope.save = function(name, content) {
+		$http.put("/api/files/" + encodeURIComponent(name), content).then(
 			function success(response) {
+				console.log("saved " + name + " successfully!");
+				$scope.dirty = false;
 			},
 			function failure(response) {
-				$scope.reset();
+				console.log("saving " + name + " failed");
 			}
 		);
 	};
@@ -54,7 +72,7 @@ app.controller('DocumentStore', function DocumentStore($scope, $http) {
 	};
 
 	$scope.setFocus = function(id) {
-		window.document.getElementById(id).focus();
+		$scope.getById(id).focus();
 	};
 
 	$scope.reset = function() {
@@ -63,23 +81,25 @@ app.controller('DocumentStore', function DocumentStore($scope, $http) {
 		$scope.size = undefined;
 		$scope.modified = undefined;
 		$scope.dirty = false;
-		$scope.close();
 		$scope.setFocus("content");
 	};
 
 	$scope.open = function() {
-		$scope.opening = true;
-		$scope.list();
-	};
+		$scope.list(function(docs) {
+			$scope.docs = docs;
+		});
 
-	$scope.close = function() {
-		$scope.opening = false;
-		$scope.docs = [ ];
-		$scope.setFocus("content");
-	}
+		var dlg = $uibModal.open({
+			animation: false,
+			templateUrl: 'docs.html',
+			controller: 'OpenDocsCtrl',
+			scope: $scope,
+			size: 'lg'
+		});
 
-	$scope.save = function() {
-		$scope.list();
+		dlg.result.then(function (selected) {
+			$scope.load(selected);
+		});
 	};
 
 	$scope.reload = function() {
@@ -94,6 +114,82 @@ app.controller('DocumentStore', function DocumentStore($scope, $http) {
 		$http.get("/api/shutdown");
 	};
 
-	$scope.docs = [ ];
+	$scope.enter_settings = function() {
+		$scope.settings = true;
+	};
+
+	$scope.exit_settings = function() {
+		$scope.settings = false;
+	};
+
+	$scope.text_size = function(delta) {
+		if (delta == 0 || delta == undefined) {
+			console.log("reset font size!");
+			$scope.text.size = $scope.defaults.text.size;
+		} else {
+			$scope.text.size = Math.round($scope.text.size + delta);
+			$scope.text.size = Math.max($scope.text.size, $scope.defaults.text.minSize);
+			$scope.text.size = Math.min($scope.text.size, $scope.defaults.text.maxSize);
+		}
+	};
+
+	$scope.confirm = function(title, message) {
+		var dlg = $uibModal.open({
+			animation: false,
+			templateUrl: 'prompt.html',
+			controller: 'PromptCtrl',
+			resolve: {
+				title: function() {
+					return title;
+				},
+				message: function() {
+					return message;
+				}
+			}
+		});
+
+		var ok = false;
+		dlg.result.then(
+			function() {
+				ok = true;
+			},
+			function() {
+				ok = false;
+			}
+		);
+
+		return ok;
+	};
+
 	$scope.reset();
+});
+
+app.controller('OpenDocsCtrl', function ($scope, $uibModalInstance) {
+	$scope.select = function(doc) {
+		$uibModalInstance.close(doc.name);
+	}
+
+	$scope.delete = function(doc) {
+		if ($scope.confirm("Confirm", "Deleting document '" + doc.name + "'. Are you sure?")) {
+			// $scope.remove(doc.name);
+			doc.removed = true;
+		}
+	}
+
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+});
+
+app.controller('PromptCtrl', function ($scope, $uibModalInstance, title, message) {
+	$scope.title = title;
+	$scope.message = message;
+
+	$scope.ok = function() {
+		$uibModalInstance.close('ok');
+	}
+
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
 });
