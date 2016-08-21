203022ca1738e3bcbbe457e93fe7067549f3aa42
Tapani Otala <github@tjotala.com>
2016-08-21 14:35:17 -0700

    Exclude files we don't care about, and any subfolders

--- app/lib/documents.rb
+++ app/lib/documents.rb
@@ -1,30 +1,36 @@
 require 'document'
 
 class Documents
-	class GlobalUser
-		def username
-			''
-		end
-	end
-
 	def initialize(volume, user)
 		@volume = volume
 		@user = user || GlobalUser.new
 	end
 
 	def list
-		Dir[File.join(path, '**')].map { |path| Document.new(path) }
+		Dir[File.join(path, '*')].reject { |path| self.class.exclude?(path) }.map { |path| Document.new(path) }
 	end
 
 	def get(base_name)
 		Document.new(File.join(path, base_name))
 	end
 
-	private
-
 	def path
 		p = File.join(@volume.path, @user.username.to_s)
 		FileUtils.mkdir_p(p)
 		p
+	end
+
+	private
+
+	class GlobalUser
+		def username
+			''
+		end
+	end
+
+	class << self
+		def exclude?(path)
+			File.directory?(path) || path =~ /^(?:\..+|System Volume Information)/
+		end
 	end
 end
