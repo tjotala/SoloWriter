3f9f36c570a57a8dcd34730645e224ed33755a1f
Tapani Otala <github@tjotala.com>
2016-08-24 00:07:45 -0700

    Make sure the /var/log/solo folder exists and is writable.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-mkdir -p /var/log/solo
+sudo mkdir -p /var/log/solo
+sudo chmod a+rw /var/log/solo
 xinit $HOME/solo/bin/solo.sh > /var/log/solo/xinit.log 2>&1
--- bin/solo.sh
+++ bin/solo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mkdir -p /var/log/solo
+sudo mkdir -p /var/log/solo
+sudo chmod a+rw /var/log/solo
 matchbox-window-manager > /var/log/solo/wm.log 2>&1 &
 ruby $HOME/solo/app/server.rb > /var/log/solo/server.log 2>&1
