6be9ceeb31a1879aa1b35e96bb2efb5ffdcc7929
Tapani Otala <github@tjotala.com>
2016-08-25 22:24:05 -0700

    Oops, forgot the spec helper enhancements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,26 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '..', 'app', 'platform')
 require 'fakefs/spec_helpers'
+require 'securerandom'
+
+class Uuid
+	UUID_PATTERN = /^\h{8}-\h{4}-4\h{3}-[89ab]\h{3}-\h{12}$/.freeze
+
+	class << self
+		def new_uuid
+			SecureRandom.uuid
+		end
+
+		def valid?(uuid)
+			uuid =~ UUID_PATTERN
+		end
+	end
+end
+
+RSpec::Matchers.define :be_a_uuid do
+	match do |actual|
+		Uuid::valid?(actual)
+	end
+end
 
 RSpec.configure do |config|
 	config.include FakeFS::SpecHelpers
