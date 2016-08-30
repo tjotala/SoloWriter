8ab7145ba455f31512962200c5de5bc575056d69
Tapani Otala <github@tjotala.com>
2016-08-29 21:16:48 -0700

    Seriously? Midori fails to launch without network connection if the hostname is "localhost" as opposed to 127.0.0.1, despite /etc/hosts resolving that...

--- bin/solo.sh
+++ bin/solo.sh
@@ -5,9 +5,10 @@ matchbox-window-manager 2>&1 &
 log=/var/log/solo/browser.log
 rm $log
 until [ `curl -s http://localhost:8080/api/ping` = 'ok' ]; do
-	echo waiting for server to launch... >> $log
+	echo `date`: waiting for server to launch... >> $log
+	sleep 1
 done
-midori -e Fullscreen -a http://localhost:8080 >> $log 2>&1
+midori -e Fullscreen -a http://127.0.0.1:8080 >> $log 2>&1
 
 #
 # alternatively, using kweb3:
