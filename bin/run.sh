c6b00d03ddc0b87250c285ad5e7331f1613b3945
Tapani Otala <github@tjotala.com>
2016-08-30 15:05:24 -0700

    Nevermind killing the browser when we quit the server. It's only gonna be the shutdown anyway.

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-(ruby $HOME/solo/app/server.rb ; killall midori kweb3) > /var/log/solo/server.log 2>&1 &
+ruby $HOME/solo/app/server.rb > /var/log/solo/server.log 2>&1 &
 xinit $HOME/solo/bin/solo.sh > /var/log/solo/xinit.log 2>&1
